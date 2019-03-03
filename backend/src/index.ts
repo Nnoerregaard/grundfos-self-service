6795ddc5557e0060ff3aaf971a0fd62763ef0dc9
Niklas Nørregaard <niklas.noerregaard@gmail.com>
2019-03-03 10:44:55 +0100

    Finally the mount works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,55 +1,41 @@
-
-//const http = require('http');
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require("mongodb").MongoClient;
 
-const hostname = '0.0.0.0';
+const hostname = "0.0.0.0";
 const port = 3000;
 
-// NB For debug usage outside of compose
-/*
-app.get('/', (req: any, res: any) => {
-    res.send("Hello world!");
-});
-
-app.post('/test', (req: any, res: any) => {
-
-});
-
-app.listen(port, hostname);
-console.log("Backend is now listening on port: " + port);
-*/
-
-
 // Connection URL
-var url = 'mongodb://db:27017/myproject';
+var url = "mongodb://db:27017/myproject";
+var test = "Hello asdfa ds!";
+var test2 = "afdasdfas";
 // Use connect method to connect to the Server
-MongoClient.connect(url, function (err: any, db: any) {
-    if (err == null) {
-        console.log("Connected correctly to database!");
+MongoClient.connect(url, function(err: any, db: any) {
+  if (err == null) {
+    console.log("Connected correctly to database!");
 
-        app.get('/', (req: any, res: any) => {
-            res.send("Hello world!");
-        });
+    app.get("/", (req: any, res: any) => {
+      res.send("Hello world!");
+    });
 
-        app.post('/test', (req: any, res: any) => {
-            
-            db.insertOne({test: req.body.testText});
-        });
+    app.post("/test", (req: any, res: any) => {
+      db.insertOne({ test: req.body.testText });
+      res.send(200);
+    });
 
-        app.get('/test', (req: any, res: any) => {
-            res.send(db.find({}));
-        })
+    app.get("/test", (req: any, res: any) => {
+      res.send(db);
+      //res.send(db.find({}));
+    });
 
-        app.listen(port);
-        console.log("Backend is now listening on port: " + port);
-    } else {
-        console.log("During connection to MongoDB: " + err);
-    }
+    app.listen(port);
+    console.log("Backend is now listening on port: " + port);
+  } else {
+    console.log("During connection to MongoDB: " + err);
+  }
 });
 
 /* Because it runs in a Docker container. 0.0.0.0 means all interfaces (including the ip in the container mapping to localhost on the host machine)
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component, Props } from 'react';
-import './App.css';
+import React, { Component, Props } from "react";
+import "./App.css";
 
-import FrontpageCard from './Components/FrontpageCard';
-import bookingImage from './resources/booking.svg';
+import FrontpageCard from "./Components/FrontpageCard";
+import bookingImage from "./resources/booking.svg";
 
-import withTopbar from './Components/HigherOrderComponents/withTopbar';
-import { Booking } from './Components/Booking';
+import withTopbar from "./Components/HigherOrderComponents/withTopbar";
+import { Booking } from "./Components/Booking";
 
-import FacebookLogin, { ReactFacebookLoginInfo } from 'react-facebook-login';
+import FacebookLogin, { ReactFacebookLoginInfo } from "react-facebook-login";
 
 interface AppState {
   isLoggedIn: boolean;
@@ -16,49 +16,61 @@ interface AppState {
 }
 
 class App extends Component<Object, AppState> {
-  constructor(props: Props<object>){
+  constructor(props: Props<object>) {
     super(props);
 
-    this.state = {isLoggedIn: false, userId: "", selectedCard: ""};
+    this.state = { isLoggedIn: false, userId: "", selectedCard: "" };
 
     this.goBack = this.goBack.bind(this);
 
-    let googleCalendarApiKey = 'AIzaSyC5r7q3hK9N7gK0ymKgHybpQ5imQH7HEEw';
-    let googleCalendarClientId = '2240079267-j91qp9qul4bqqu622o1kmsvbkjv7qoum.apps.googleusercontent.com';
-
+    let googleCalendarApiKey = "AIzaSyC5r7q3hK9N7gK0ymKgHybpQ5imQH7HEEw";
+    let googleCalendarClientId =
+      "2240079267-j91qp9qul4bqqu622o1kmsvbkjv7qoum.apps.googleusercontent.com";
   }
 
-  selectCard(selectedCard: string){
-    this.setState({selectedCard: selectedCard});
+  selectCard(selectedCard: string) {
+    this.setState({ selectedCard: selectedCard });
   }
 
-  goBack(){
-    this.setState({selectedCard: ""});
+  goBack() {
+    this.setState({ selectedCard: "" });
   }
 
   render() {
     const responseFacebook = (response: ReactFacebookLoginInfo) => {
-      this.setState({isLoggedIn: true, userId: response.id});
-    }
+      this.setState({ isLoggedIn: true, userId: response.id });
+    };
 
-    const BookingWithTopbar = withTopbar(Booking, {title: "Fællesrumsbooking", goBack: this.goBack});
+    const BookingWithTopbar = withTopbar(Booking, {
+      title: "Fællesrumsbooking",
+      goBack: this.goBack
+    });
 
     return (
       <div className="App">
         {/* Login part */}
         <div hidden={this.state.isLoggedIn}>
-        <FacebookLogin 
-          appId="783077602052331"
-          autoLoad={false}
-          fields="name"
-          callback={responseFacebook} />
+          <FacebookLogin
+            appId="783077602052331"
+            autoLoad={false}
+            fields="name"
+            callback={responseFacebook}
+          />
         </div>
 
         {/* Main part */}
         <div hidden={!this.state.isLoggedIn}>
           {/* The navigation part */}
-          <div hidden={this.state.selectedCard != ""} onClick={this.selectCard.bind(this, "booking")}>
-            <FrontpageCard title="Booking" image={bookingImage} />
+          <div
+            hidden={this.state.selectedCard != ""}
+            onClick={this.selectCard.bind(this, "booking")}
+          >
+            <FrontpageCard title="Booking af fællesrum" image={bookingImage} />
+            <FrontpageCard
+              title="Reservation af støvsugere"
+              image={bookingImage}
+            />
+            <FrontpageCard title="Kontakt beboerrådet" image={bookingImage} />
           </div>
 
           {/* The content part */}
